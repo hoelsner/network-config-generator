99463386a15bd4d75b09033b1d2a2b979fe38dac
Henry Ölsner <henry@codingnetworker.com>
2015-12-29 14:56:06 +0100

    add logging to the run server script

--- run_server.py
+++ run_server.py
@@ -1,10 +1,35 @@
 #!python3
 """
-Run the flask application locally. The configuration is defined using the `APP_SETTINGS` environment variable (defaults
-to `config.DefaultConfig`. If you need to enable the Debug mode, set the environment variable `DEBUG_MODE` to 1.
+Start a local Flask development server. The configuration is defined using the `APP_SETTINGS` environment variable
+(defaults to `config.DefaultConfig`, if you need to enable the debug mode, set the environment variable `DEBUG_MODE`
+to 1).
 """
+import logging
 import os
+
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('DEBUG_MODE', False))
+    logging_directory = "log"
+    debug_mode = os.getenv('DEBUG_MODE', False)
+
+    if not os.path.exists(logging_directory):
+        os.mkdir(logging_directory)
+
+    if debug_mode:
+        level = logging.DEBUG
+
+    else:
+        level = logging.INFO
+
+    logFormatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    logging.basicConfig(filename=os.path.join(logging_directory, 'application.log'), level=level)
+
+    # add console handler
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(logFormatter)
+    logging.getLogger().addHandler(stream_handler)
+
+    logging.getLogger().debug("Start debugging...")
+
+    app.run(host='0.0.0.0', debug=debug_mode)
