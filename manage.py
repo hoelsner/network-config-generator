935059b64fdd1ba96c6bdff374d3fb5c8c567e21
Henry Ölsner <henry@codingnetworker.com>
2016-02-09 21:47:40 +0100

    add runserver command

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 #!python3
 import os
-from flask.ext.script import Manager
+from flask.ext.script import Manager, Server
 from flask.ext.migrate import Migrate, MigrateCommand
 from app import app, db
 
@@ -10,6 +10,13 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
+
+manager.add_command('runserver', Server(
+    use_debugger=os.getenv('DEBUG_MODE', True),
+    use_reloader=os.getenv('FLASK_RELOADER', True),
+    host=os.getenv("FLASK_IP", "0.0.0.0"),
+    port=int(os.getenv("FLASK_PORT", 5000))
+))
 
 if __name__ == '__main__':
     manager.run()
